afb401cc71d944b496177b80e68fb7eec52b9959
pstech965 <pstech965@gmail.com>
2026-03-30 13:14:59 +0530

    Update college name generation function
    Refactor college name generation logic and API integration.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const API_KEY = 'AIzaSyBtHBBi_67DKnaiCUfjMJOiDYy8sjrJp84'
+const API_KEY = ''
 
 async function generateContent(prompt) {
 
     const response = await fetch(
-        "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent",
+        "Your API Key Link",
         {
             method: "POST",
             headers: {
